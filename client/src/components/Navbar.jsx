5d79f194c5885a5bf4c944c8f67d0e41872def58
Nathan Prater <nprater86@gmail.com>
2022-07-11 16:23:09 -0500

    updated projects section

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Nav, Navbar, Container } from 'react-bootstrap';
 import GitHub_Logo_White from '../images/GitHub_Logo_White.png';
+import GitHub_Mark_Light from '../images/GitHub-Mark-Light-120px-plus.png';
+import LI_ln_Bug from '../images/LI-In-Bug.png';
 import LI_Logo from '../images/LI_Logo.png';
 
 const Navigation = props => {
@@ -9,6 +11,22 @@ const Navigation = props => {
     return (
         <Navbar variant="dark" expand="lg" className="mb-3 border-bottom" fixed="top" id="navbar" collapseOnSelect>
             <Container className="d-flex justify-content-between">
+                <div className="d-none d-sm-flex">
+                    <Nav.Link href="https://github.com/nprater86" target="_blank">
+                        <img style={{height: '35px'}} src={GitHub_Logo_White} alt="Link to Nathan Prater's Github" />
+                    </Nav.Link>
+                    <Nav.Link href="https://www.linkedin.com/in/nathanprater" target="_blank">
+                        <img style={{height: '28px'}} src={LI_Logo} alt="Link to Nathan Prater's Github" />
+                    </Nav.Link>
+                </div>
+                <div className="d-flex d-sm-none">
+                    <Nav.Link href="https://github.com/nprater86" target="_blank">
+                        <img style={{height: '35px'}} src={GitHub_Mark_Light} alt="Link to Nathan Prater's Github" />
+                    </Nav.Link>
+                    <Nav.Link href="https://www.linkedin.com/in/nathanprater" target="_blank">
+                        <img style={{height: '28px'}} src={LI_ln_Bug} alt="Link to Nathan Prater's Github" />
+                    </Nav.Link>
+                </div>
                 <div>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
@@ -19,14 +37,6 @@ const Navigation = props => {
                             <Nav.Link data-menuanchor="contact" href="#contact">contact</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
-                </div>
-                <div className="d-flex">
-                    <Nav.Link href="https://github.com/nprater86" target="_blank">
-                        <img style={{height: '35px'}} src={GitHub_Logo_White} alt="Link to Nathan Prater's Github" />
-                    </Nav.Link>
-                    <Nav.Link href="https://www.linkedin.com/in/nathanprater" target="_blank">
-                        <img style={{height: '28px'}} src={LI_Logo} alt="Link to Nathan Prater's Github" />
-                    </Nav.Link>
                 </div>
             </Container>
         </Navbar>
--- client/src/views/Projects.jsx
+++ client/src/views/Projects.jsx
@@ -8,10 +8,14 @@ const Projects = props => {
     const [imgDisplay, setImgDisplay] = useState('none');
     const [src, setSrc] = useState();
 
-    const popup = document.getElementById('popup');7
+    const videoPlayer = document.getElementById("videoPlayer");
 
     document.addEventListener('wheel', (e) => {
-        setTimeout(() => { setImgDisplay('none'); }, 700);
+        
+        setTimeout(() => { 
+            videoPlayer.src = videoPlayer.src;
+            setImgDisplay('none'); 
+        }, 700);
     })
 
     function imgDisplayOpen(src) {
@@ -20,6 +24,7 @@ const Projects = props => {
     }
 
     function imgDisplayClose() {
+        videoPlayer.src = videoPlayer.src;
         setImgDisplay('none');
     }
 
@@ -35,58 +40,62 @@ const Projects = props => {
                         <img onClick={ e => imgDisplayOpen("https://www.youtube.com/embed/L2REJARYuYQ") } className="img-thumbnail mb-3 thumbnail" src={drinkmixr} alt="" />
                     </div>
                     <div className="col-4">
-                        <img onClick={ e => imgDisplayOpen(dojoweather) } className="img-thumbnail mb-3 thumbnail" src={dojoweather} alt="" />
+                        <img onClick={ e => imgDisplayOpen("https://www.youtube.com/embed/7aLRZ2AXgLc") } className="img-thumbnail mb-3 thumbnail" src={dojoweather} alt="" />
                     </div>
                     <div className="col-4">
-                        <img onClick={ e => imgDisplayOpen(storefronts) } className="img-thumbnail mb-3 thumbnail" src={storefronts} alt="" />
+                        <img onClick={ e => imgDisplayOpen("https://www.youtube.com/embed/lbN6KBkn_V8") } className="img-thumbnail mb-3 thumbnail" src={storefronts} alt="" />
                     </div>
                 </div>
                 <div className="row justify-content-between">
                     <div className="col-4">
                         <h5 className="mb-3">DrinkMixr</h5>
                         <p>Social media site for aspiring mixologists. Browse recipes, comment on them, add them to your favorites, or create your own! Built using Python, Flask, and MySQL.</p>
-                        <a href="https://www.youtube.com/watch?v=L2REJARYuYQ">Demo</a>
+                        <a href="https://www.youtube.com/watch?v=L2REJARYuYQ" target="_blank">Demo</a>
                     </div>
                     <div className="col-4">
                         <h5 className="mb-3">Dojo Weather</h5>
                         <p>Simple and fast, check on the weather in any city in the US. Register to gain access to a dashboard where you can save locations for ease of access. Built using MongoDB, Express, React, and Node.js. Powered by the OpenWeather and Google Maps APIs.</p>
+                        <a href="https://www.youtube.com/watch?v=7aLRZ2AXgLc" target="_blank">Demo</a>
                     </div>
                     <div className="col-4">
                         <h5 className="mb-3">Dojo Storefronts</h5>
                         <p>E-commerce website where users can browse storefronts and buy products from a variety of sellers, or create a storefront of their own! Built using C#, ASP .NET, and MySQL.</p>
+                        <a href="https://www.youtube.com/watch?v=lbN6KBkn_V8" target="_blank">Demo</a>
                     </div>
                 </div>
                 <div className="position-absolute top-50 start-50 translate-middle px-5 pb-5 pt-4 w-100 border flex-column rounded" style={{backgroundColor: 'black', display:imgDisplay}} id="popup">
                     <i onClick={ e => imgDisplayClose() } className="fa-solid fa-xmark mb-3 align-self-end thumbnail"></i>
-                    <div style={{paddingBottom: "56.25%"}}>
-                        <iframe style={{width:"500px", height: "100%"}} src={src} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <div>
+                        <iframe id="videoPlayer" style={{width:"100%", height: "65vh"}} src={src} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                     </div>
                 </div>
             </div>
             <div className="row d-md-none">
                 <div className="row align-items-center">
                     <div className="col-6">
-                    <a href="https://www.youtube.com/watch?v=L2REJARYuYQ"><img className="img-thumbnail mb-3 thumbnail" src={drinkmixr} alt="" /></a>
+                        <a href="https://www.youtube.com/watch?v=L2REJARYuYQ" target="_blank"><img className="img-thumbnail mb-3 thumbnail" src={drinkmixr} alt="" /></a>
                     </div>
                     <div className="col-6">
                         <h6 className="mb-3">DrinkMixr</h6>
-                        <a href="https://www.youtube.com/watch?v=L2REJARYuYQ">Demo</a>
+                        <a href="https://www.youtube.com/watch?v=L2REJARYuYQ" target="_blank">Demo</a>
                     </div>
                 </div>
                 <div className="row align-items-center">
                     <div className="col-6">
-                    <img className="img-thumbnail mb-3 thumbnail" src={dojoweather} alt="" />
+                        <a href="https://www.youtube.com/watch?v=7aLRZ2AXgLc" target="_blank"><img className="img-thumbnail mb-3 thumbnail" src={dojoweather} alt="" /></a>
                     </div>
                     <div className="col-6">
                         <h6 className="mb-3">Dojo Weather</h6>
+                        <a href="https://www.youtube.com/watch?v=7aLRZ2AXgLc" target="_blank">Demo</a>
                     </div>
                 </div>
                 <div className="row align-items-center">
                     <div className="col-6">
-                        <img className="img-thumbnail mb-3 thumbnail" src={storefronts} alt="" />
+                    <a href="https://www.youtube.com/watch?v=lbN6KBkn_V8" target="_blank"><img className="img-thumbnail mb-3 thumbnail" src={storefronts} alt="" /></a>
                     </div>
                     <div className="col-6">
                         <h6 className="mb-3">Dojo Storefronts</h6>
+                        <a href="https://www.youtube.com/watch?v=lbN6KBkn_V8" target="_blank">Demo</a>
                     </div>
                 </div>
             </div>
