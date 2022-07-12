f861780c4fb17efef62f5959e386df1d34c351fc
Nathan Prater <nprater86@gmail.com>
2022-07-12 12:07:56 -0500

    downgraded the contact me section

--- client/src/views/Contact.jsx
+++ client/src/views/Contact.jsx
@@ -9,7 +9,21 @@ const Contact = props => {
                 <h2 className="d-inline">contact me</h2>
                 <img className="pb-2" src={blinking_cursor} alt="blinking cursor" />
             </div>
-            <ContactForm />
+            {/* <ContactForm /> */}
+            <div className="row gap-5 d-none d-md-flex">
+                <h6 className="mb-0">Voicemail: <span className="roboto">‪(773) 236-0503‬</span></h6>
+                <h6 className="mb-0">Email: <span className="roboto">nprater86@gmail.com</span></h6>
+            </div>
+            <div className="row gap-5 d-md-none">
+                <div className="row gap-2">
+                    <h6 className="mb-0">Voicemail:</h6>
+                    <span className="roboto">‪(773) 236-0503‬</span>
+                </div>
+                <div className="row gap-2">
+                    <h6 className="mb-0">Email:</h6>
+                    <span className="roboto">nprater86@gmail.com</span>
+                </div>
+            </div>
         </div>
     );
 }
