53d50666097473e2e32bad17018e4aba30180dba
Nathan Prater <nprater86@gmail.com>
2022-07-11 20:00:08 -0500

    skillset

--- client/src/views/SkillSet.jsx
+++ client/src/views/SkillSet.jsx
@@ -4,11 +4,11 @@ import blinking_cursor from '../images/blinking_cursor.gif';
 const SkillSet = props => {
     return (
         <div id="skillSetSection">
-            <div className="mb-5 mt-5">
-                <h2 className="d-inline">skill set</h2>
-                <img className="pb-2" src={blinking_cursor} alt="blinking cursor" />
-            </div>
             <div className="row d-none d-lg-flex">
+                <div className="mb-5 mt-5">
+                    <h2 className="d-inline">skill set</h2>
+                    <img className="pb-2" src={blinking_cursor} alt="blinking cursor" />
+                </div>
                 <div className="col-3">
                     <h5>Languages</h5>
                     <ul>
@@ -57,20 +57,25 @@ const SkillSet = props => {
                     </ul>
                 </div>
             </div>
-            <div className="row d-lg-none gap-5">
-                <div className="row">
+            <div className="row d-lg-none gap-2">
+                <div>
+                    <h2 className="d-inline">skill set</h2>
+                    <img className="pb-2" src={blinking_cursor} alt="blinking cursor" />
+                </div>
+                <div>
                     <h6>Languages</h6>
                     <p className="roboto">C# | Python | JavaScript (ES6, Node.js) | HTML5 | CSS</p>
                 </div>
-                <div className="row">
-                    <h6>Frameworks/Libraries</h6>
+                <div>
+                    <h6>Frameworks</h6>
+                    <h6>/Libraries</h6>
                     <p className="roboto">ASP .NET | Flask | Jinja2 | React | Express | Bootstrap | Material-UI</p>
                 </div>
-                <div className="row">
+                <div>
                     <h6>Databases</h6>
                     <p className='roboto'>Entity Framework Core | MySQL | MongoDB | Mongoose</p>
                 </div>
-                <div className="row">
+                <div>
                     <h6>Tools</h6>
                     <p className='roboto'>AJAX | AWS(EC2) | MySQL Workbench | JSON | Postman | MongoDBCompass | Balsamiq | Git | GitHub | GitBash</p>
                 </div>
